9ae0ecf86c00cabd0ecaf22962e2ff7baa7b529e
Wildtsk <wildtsk@gmail.com>
2022-10-20 14:18:19 +0300

    20

--- part1/practice/bad_smell_big_class/main.py
+++ part1/practice/bad_smell_big_class/main.py
@@ -9,8 +9,6 @@
 # Дерево может защищаться (попробуй разруби сходу) и гореть в огне
 # Ловушка не может ничего кроме как атаковать того, кто на нее наступит
 # Для решения этой задачи не используйте наследование
-
-class Obj:
     ##
     # тут представлено поведение четырех различных игровых объектов:
     # - воина
@@ -18,6 +16,7 @@ class Obj:
     # - дерева
     # - ловушки
 
+class Warrior:
     def attack(self):
         pass
 
@@ -27,6 +26,8 @@ class Obj:
     def move(self):
         pass
 
+
+class Healer:
     def healer_defense(self):
         pass
 
@@ -36,14 +37,19 @@ class Obj:
     def heal(self):
         pass
 
+
+class Tree:
     def tree_defense(self):
         pass
 
     def on_fire(self):
         pass
 
+
+class Trap:
     def trap_attack(self):
         print("It's a trap!")
+
 
 if __name__ == '__main__':
     unit = Warrior()
